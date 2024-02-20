e8068aa16f20c3a9834075a6cc019d303652dd2d
Mikeab <97768019+MikeAchoy@users.noreply.github.com>
2024-02-20 10:46:46 -0600

    Edited TODOS
    Wrote TODOS, and deleted completed TODOS.

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,39 +2,40 @@
 import java.util.concurrent.TimeUnit;
 //import javax.swing.SwingUtilities;
 
-public class Main{
-    public static void main(String [] args){
-        /*
-        SwingUtilities.invokeLater(() -> {
-            Window window = new Window();
-            window.setWindowVisible(true);
+public class Main  
+  blic static void main(String[] args) {
+    /*
+     * SwingUtilities.invokeLater(() -> {
+     * Window window = new Window();
+     * window.setWindowVisible(true);
+     * 
+     * try {
+     * TimeUnit.MILLISECONDS.sleep(3000);
+     * // Do something here
+     * window.repaint();
+     * } catch (InterruptedException e) {
+     * e.printStackTrace();
+     * }
+     * });
+     */
 
-            try {
-                TimeUnit.MILLISECONDS.sleep(3000);
-                // Do something here
-                window.repaint();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-         */
+    // TODO: Add more sorting algorithms to the project.
+    // TODO: Implement quick sort algorithm written in the algorithms file.
+    Window window = new Window();
+    window.setWindowVisible(true);
 
-        // TODO: Add more sorting algorithms to the project.
-        Window window = new Window();
-        window.setWindowVisible(true);
-        
-        /*
-         
-
-        try {
-            TimeUnit.SECONDS.sleep(2);
-            // Do something here
-            window.startVisualization();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-         */
-        // Start example visualization after half a second
-    }
+    /*
+     * 
+     * 
+     * try {
+     * TimeUnit.SECONDS.sleep(2);
+     * // Do something here
+     * window.startVisualization();
+     * } catch (InterruptedException e) {
+     * e.printStackTrace();
+     * }
+     * 
+     */
+    // Start example visualization after half a second
+  }
 }
